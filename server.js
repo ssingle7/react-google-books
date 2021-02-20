ab65c22a2be349b808fdac9dc6d75e81a3eeb2ec
dlhardi <derricklhardison@gmail.com>
2021-02-20 17:21:31 -0500

    Changed name of database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("client/build"));
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/mern-starter", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
