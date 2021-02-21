fad072867a24d6d8853bbe240c11c35e481db981
Selena Singleton <ssingle7@gmail.com>
2021-02-20 19:56:34 -0500

    created header and hero components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Hero />
     </div>
   );
 }
