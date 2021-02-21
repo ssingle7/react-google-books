ea826d1aa2112f00184e038c6fef80b41f9b7415
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-20 21:17:06 -0500

    add book search component

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import BookSearch from "../components/BookSearch/BookSearch";
 
 const Search = () => {
-    return (
-        <div>
-            <h1>This is my search page</h1>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>This is my search page</h1>
+          <BookSearch />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Search;
---
+++ client/src/components/BookSearch/BookSearch.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
+
+const BookSearch = () => {
+  return (
+    <>
+      <form>
+        <div class="mb-3">
+          <label for="searchBook" class="form-label">
+            Search for Book
+          </label>
+          <input
+            //   onChange
+            //   value
+            type="text"
+            class="form-control mb-3 "
+            id="searchBook"
+          />
+          <button type="submit" class="btn btn-primary float-right">
+            Search
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default BookSearch;
