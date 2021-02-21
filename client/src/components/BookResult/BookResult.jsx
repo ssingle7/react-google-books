e3519420ce381d6910ae75e8ce6007d92d8aa6c9
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-20 22:39:48 -0500

    added styling

--- client/src/components/BookResult/BookResult.jsx
+++ client/src/components/BookResult/BookResult.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import "./BookResult.css"
 
 const BookResult = () => {
   return (
     <div className="mt-5">
-        <br /><hr />
+      <br />
+      <hr />
       <h1>Results</h1>
 
       <div className="card">
         <div className="card-body">
-        <div id="buttons" className="float-right">
+          <div id="buttons" className="float-right">
             <a href="#" className="btn btn-primary mr-3">
               VIEW
             </a>
@@ -18,7 +20,6 @@ const BookResult = () => {
           </div>
           <h5 className="card-title">Title</h5>
 
-
           <p className="card-text">Author</p>
         </div>
         <div className="container">
@@ -26,19 +27,38 @@ const BookResult = () => {
             <div className="col-12">
               <div className="card mb-3">
                 <div className="row no-gutters">
-                  <div className="col-md-3">
+                  <div className="col-md-5">
                     <img
+                        className="full-img"
                       src="https://picsum.photos/200/200?grayscale"
                       alt="..."
                     />
                   </div>
-                  <div className="col-md-9">
+                  <div className="col-md-7">
                     <div className="card-body">
-                      <h5 className="card-title">Card title</h5>
                       <p className="card-text">
-                        This is a wider card with supporting text below as a
-                        natural lead-in to additional content. This content is a
-                        little bit longer.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Architecto quas pariatur, repudiandae, possimus animi
+                        natus autem eius libero voluptate velit placeat illum
+                        eum atque reprehenderit sit mollitia, quidem
+                        consequuntur! Animi consequatur a laborum eligendi
+                        repellendus ut suscipit iure eaque. Iste molestiae
+                        facere tenetur ducimus incidunt vero iusto rerum dolore!
+                        Nulla ea magnam molestiae, aliquid obcaecati unde, quos
+                        odio voluptate ratione sunt nobis consectetur, in esse
+                        et enim autem sint error eius corporis excepturi quaerat
+                        deserunt! Veritatis dolorum quod vitae! Quod consectetur
+                        quo numquam iste ducimus? Reprehenderit iste pariatur
+                        temporibus dolor est voluptate, sed repellendus!
+                        Blanditiis expedita culpa cumque magni impedit molestias
+                        explicabo exercitationem? Consequatur unde perferendis,
+                        accusantium distinctio recusandae ratione numquam aut
+                        facere iusto officiis pariatur at corporis soluta
+                        voluptatum, sit reiciendis? Inventore dolor voluptate
+                        accusantium, nostrum tenetur a, quasi, dolore itaque
+                        facilis eveniet quia illo beatae recusandae
+                        reprehenderit? Sunt eligendi eius ipsa unde blanditiis
+                        laboriosam vitae eaque perferendis fugit.
                       </p>
                     </div>
                   </div>
