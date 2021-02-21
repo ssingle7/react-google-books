eae37d830ebfbbfe4a88c00a703f3c9f126d3e08
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-20 22:13:25 -0500

    added footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-5">
+      <footer class="footer mt-auto py-3 bg-dark">
+        <div class="container">
+          <span class="text-muted">&copy;2021. React Google Books</span>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
