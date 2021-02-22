956e68e13036672030ca358e78a9eda2a59981f3
dlhardi <derricklhardison@gmail.com>
2021-02-21 19:53:54 -0500

    Testing API calls

--- client/src/components/BookSearch/BookSearch.jsx
+++ client/src/components/BookSearch/BookSearch.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+// const query =  https://www.googleapis.com/books/v1/volumes?q=intitle:{formValue}&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w
+
+function SearchBooks() {
+ 
+
+  axios
+    .get(
+      `https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
+    )
+    .then((response) => {
+       console.log(response);
+      //  console.log(response.data.items[0].volumeInfo.title);
+      //  console.log(response.data.items[0].volumeInfo.authors[0]);
+      //  console.log(response.data.items[0].volumeInfo.imageLinks.thumbnail);
+    });
+}
 
 const BookSearch = () => {
+
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleInputChange = event => {
+    const { value} = event.target;
+    setBookSearch(value)
+  }
+
+
+  useEffect(() => {
+    SearchBooks();
+  }, []);
+
   return (
     <div className="mb-5">
-      <form>
+      <form onSubmit={SearchBooks()}>
         <div className="mb-3">
           <label htmlFor="searchBook" className="form-label">
             Search for Book
           </label>
           <input
-            //   onChange
-            //   value
+            onChange={handleInputChange}
+            value={bookSearch}
             type="text"
             className="form-control mb-3 "
             id="searchBook"
