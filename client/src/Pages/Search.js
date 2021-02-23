87dc733c18d0405fc20f202b43795fd9661145cf
dlhardi <derricklhardison@gmail.com>
2021-02-23 18:39:20 -0500

    Modified file structure. Added routes fodler and index.js, added index.js to models

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BookSearch from "../components/BookSearch/BookSearch";
 import BookResult from "../components/BookResult/BookResult";
+import axios from "axios";
 
-
-
+function BookInfo(book) {
+  return (
+    // <BookSearch />
+    console.log()
+  );
+}
 
 const Search = () => {
+  const [books, setBooks] = useState([]);
+
+  function SearchBooks() {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=intitle:romeo&key=AIzaSyAfrpQXZXcliTIXdDGXtxQAW6Pi5iGb42w`
+      )
+      .then((response) => {
+        console.log(response.data.items);
+        setBooks(response.data.items);
+        //  const arrayOfBooks = response.data.items
+
+        //  console.log(arrayOfBooks);
+
+        //  arrayOfBooks.map(BookInfo)
+
+        //  const bookTitle = (response.data.items.volumeInfo.title)
+        //  console.log(bookTitle)
+
+        //  const bookAuthor = (response.data.volumeInfo.authors)
+        //  console.log(bookAuthor)
+
+        //  const bookDescription = (response.data.volumeInfo.description)
+        //  console.log(bookDescription)
+
+        //  const bookImage = (response.data.volumeInfo.imageLinks.thumbnail)
+        //  console.log(bookImage)
+
+        //  const bookLink = (response.data.saleInfo.buyLink)
+        //  console.log(bookLink)
+        //  console.log(response.data.items[0].volumeInfo.title);
+        //  console.log(response.data.items[0].volumeInfo.authors[0]);
+        //  console.log(response.data.items[0].volumeInfo.imageLinks.thumbnail);
+      });
+  }
+
+  useEffect(() => {
+    SearchBooks();
+  }, [])
+
   return (
     <div className="container">
       <div className="row">
@@ -14,7 +59,11 @@ const Search = () => {
             <h1>Book Search</h1>
             <BookSearch />
           </div>
-          <BookResult />
+          {books.map((book) => (
+            <BookResult {...book} 
+              key={book.id}
+            />
+          ))}
         </div>
       </div>
     </div>
